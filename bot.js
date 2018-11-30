c3b2d76721c7e37a4bb84015006481d86cb89381
bankroom <45100686+bankroom@users.noreply.github.com>
2018-12-01 01:09:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
   
  client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', '¥chat');
+    let channel = member.guild.channels.find('name', '𝐖𝐄𝐋𝐂𝐎𝐌𝐄');
     let memberavatar = member.user.avatarURL
       if (!channel) return;
     let embed = new Discord.RichEmbed()
@@ -27,7 +27,7 @@ client.on('ready', () => {
 client.on("message", message => {
 var prefix = "*";//البرفكس
         if(message.content.startsWith(prefix + "say")) {
-    if(message.author.id !== "490688243864567834") return message.reply("هذا الامر لصحاب البوت فقط");
+    if(message.author.id !== "518184058031833141") return message.reply("هذا الامر لصحاب البوت فقط");
         let args = message.content.split(" ").slice(1);
 message.channel.send(args)
  message.delete()
